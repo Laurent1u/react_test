03da0a06f6cbb6ada0ed7e099f6d0293febb927d
Larry <pascu.laurentiu17@gmail.com>
2019-11-26 15:21:54 +0200

    create build folder

---
+++ build/precache-manifest.17bf8a95b38cdb98325446d66a4a6bf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c573cd1f02f8ba17edc743bc660d25c7",
+    "url": "/react_test/index.html"
+  },
+  {
+    "revision": "b9063240f2f108a5287e",
+    "url": "/react_test/static/css/main.b932388d.chunk.css"
+  },
+  {
+    "revision": "5e8b37e5fa49f7029404",
+    "url": "/react_test/static/js/2.0d528225.chunk.js"
+  },
+  {
+    "revision": "b9063240f2f108a5287e",
+    "url": "/react_test/static/js/main.0c1dbac3.chunk.js"
+  },
+  {
+    "revision": "08d4673a26e83d780883",
+    "url": "/react_test/static/js/runtime-main.ce58dc4d.js"
+  }
+]);
