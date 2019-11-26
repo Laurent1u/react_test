178d514837fb988ffc528d882a7949c67d9e8335
Larry <pascu.laurentiu17@gmail.com>
2019-11-26 15:29:43 +0200

    add all files

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
+
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Larry', age: 27, belt: 'black', id: 1 },
+      { name: 'Ana', age: 28, belt: 'white', id: 2 },
+      { name: 'Oana', age: 29, belt: 'red', id: 3 },
+      { name: 'Mariana', age: 30, belt: 'brown', id: 4 }
+    ]
+  }
+
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id;
+    })
+
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
+  componentDidMount() {
+    console.log('component mounted');
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('component updated');
+    console.log(prevProps, prevState);
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <h1>My first React app !</h1>
+        <p>Welcome :)</p>
+
+        <Ninjas ninjas={this.state.ninjas} deleteNinja={this.deleteNinja} />
+        <AddNinja addNinja={this.addNinja} />
+      </div>
+    );
+  }
+}
+
+export default App;
